c7650397fe708469b7769917187079668a2c920c
israaothman <israaayoub@hotmail.com>
2022-01-19 21:21:47 +0200

    demo05

---
+++ Day05/demo/script/app.js
@@ -0,0 +1,155 @@
+console.log("Hi I'm inside the js "); // check if index.html connected with the app.js 
+ /*jshd
+ jshd
+
+ comment more than one line 
+
+ jshd
+ */
+ // comment one line 
+ // ctrl + ? 
+
+//  1- value Types (primitives):
+// a. numbers
+// b. string
+// c. boolean
+
+// 2- Reference Types (Objects) :
+// a. Objects
+// b. Functions
+// c. Arrays
+
+
+ // Data types 
+ // String = "Ahmad" or 'Ahmad'
+ // Number = 20
+ // boolean = true or false 
+  
+
+ // Declare variables 
+ var myName = "Israa";
+ var age =25;
+ var task = true; 
+
+//  console.log(userName);
+// alert("Hello User ");
+// alert("I'm Isra'a");
+
+// var userName = prompt("What's your name ?");
+// console.log(userName);
+// // alert(user);
+
+// var userAge = prompt("How old are you ?");
+// console.log(userAge);
+
+// comparison operations
+// > 
+// < 
+// <=
+// >= 
+// != 
+// ==  compare the value 
+// === identical => compare the dataType and the value 
+
+// Logical operations
+// 1- And 
+// 2- OR 
+// 3- Not 
+
+
+var x = "23"; //string  (one equal means assigning value to a variable )
+var y = 23; // number 
+
+// x==y => true 
+// x===y => false 
+
+
+// if(condition){
+//     some code if the condition is true 
+// }else{
+//     false do this code 
+// }
+
+// console.log(x===y);
+// if(x===y){
+//     alert("x == y ")
+// }else{
+//     alert("x is not equal y ")
+// }
+
+
+// if(condition 1){
+//     some code if the condition 1 is true 
+// }else if(condition 2 ) {
+//    code if condition 2 is true 
+// }else {
+    // if cond 1 and cond 2 are false 
+// }
+
+// if(userAge >= 20){   
+//     alert("Welcome to my website :) ")
+// }else if(userAge <= 10){
+//    alert("Go to study")
+// } else{ // 11 - 19 
+//    alert("Hello world ")
+// }    
+
+// if(userAge >= 20 && userAge <=10 ){
+//     alert("welcome ");
+// }
+
+
+// switch(variable){
+//     case condition : 
+//        code ;
+//        break;
+//     case condition 2 :
+//         code ;
+//         break;
+
+//     default:
+//         code ;
+// }
+
+
+// var value1 = prompt("Enter a number ");
+//   if(value1 != ""){
+//     var x=value1 % 2            
+//     switch (x) {         
+//       case 0:
+//         console.log("Even number");
+//         break;
+//       case 1:
+//         console.log("Odd number");
+//         break;
+//       default:
+//         console.log("Invalid input");
+//     }
+//   }else{
+//       alert("please enter a number ")
+//   }
+
+
+var userName = prompt("Enter you name :) ")
+// Ahmad 
+// AHMAD 
+// ahmad 
+
+// var converted = userName.toLowerCase();   //ahmad
+// if(converted == "ahmad"){
+//     alert("Welcome Ahmad ");
+// }
+
+// var converted = userName.toUpperCase();   //AHMAD
+// if(converted == "AHMAD"){
+//     alert("Welcome Ahmad ");
+// }
+alert("Welcome  " + userName); //concatenation 
+
+
+
+
+
+
+
+
